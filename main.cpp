03b21a2f1f849a19da7d88719017b2c426caf350
kgoba <karlis.goba@gmail.com>
2010-01-26 10:08:01 +0000

    Added empty main.cpp file with a structure outline

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <map>
+#include <vector>
+
+#include <cstdio>
+
+int main()
+{
+	// Read options
+
+	// Choose directions
+	// For each sentence
+		// Read lattice into memory
+		// For each direction
+			// Propagate envelope through the lattice
+			// Get gamma boundaries gamma_1..gamma_n
+			// Count BLEU n-gram statistics for e_1..e_n
+	
+	// For each direction
+		// Merge gamma points
+		// Maximimize BLEU over gamma and direction
+
+	// Update lambdas
+
+	return 0;
+}
+
