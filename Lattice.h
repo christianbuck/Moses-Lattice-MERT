39e4342ea93e088ee5a1af814bd4fd9db9dc702c
kgoba <karlis.goba@gmail.com>
2010-01-27 16:35:34 +0000

    Some improvements (storing features in edges)

--- Lattice.h
+++ Lattice.h
@@ -1,20 +1,37 @@
 #pragma once
 
 #include <map>
-
+#include <vector>
 
 class Lattice {
-public:
-    void addEdge(size_t from, size_t to) {
-        vertices[from].out.push_back(to);
-        vertices[to].in.push_back(from);
-    }
-
 private:
+    typedef size_t VertexKey;
+    typedef std::pair<VertexKey, VertexKey> EdgeKey;
+
+public:
+    Lattice() : edgeCount(0) {}
+
     struct Vertex {
-        vector<size_t> in;
-        vector<size_t> out;
+        std::vector<VertexKey> in;
+        std::vector<VertexKey> out;
+    };
+    struct Edge {
+        std::vector<double> features;
     };
 
-    std::map<size_t, Vertex> vertices;
+    Edge & addEdge(VertexKey from, VertexKey to) {
+        vertices[from].out.push_back(to);
+        vertices[to].in.push_back(from);
+        Edge & edge = edges[EdgeKey(from, to)];
+        edgeCount++;
+        return edge;
+    }
+
+    size_t getVertexCount() const { return vertices.size(); }
+    size_t getEdgeCount() const { return edges.size(); }
+
+private:
+    std::map<VertexKey, Vertex> vertices;
+    std::map<EdgeKey, Edge> edges;
+    size_t edgeCount;
 };
