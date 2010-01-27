a66fcba6d8a42b518873325d8382d4224ab96396
kgoba <karlis.goba@gmail.com>
2010-01-27 19:24:51 +0000

    +test routine

--- Main.cpp
+++ Main.cpp
@@ -1,27 +1,59 @@
 #include <iostream>
-#include <map>
-#include <vector>
-
+#include <fstream>
 #include <cstdio>
+#include <string>
+#include <list>
 
-int main()
+#include "MosesGraphReader.h"
+#include "Lattice.h"
+#include "Types.h"
+
+using std::ifstream;
+using std::string;
+using std::vector;
+using std::list;
+using std::cout;
+using std::endl;
+using std::ostream;
+
+void test1()
 {
-	// Read options
+    ifstream is("/home/karlos/Moses-Lattice-MERT/osg");
+    MosesGraphReader reader(is);
+    vector<double> lambdas;
+    vector<double> dir;
+    for (size_t i = 0; i < 15; i++) {
+        lambdas.push_back(0.5);
+        dir.push_back(0.02);
+    }
+    while (true) {
+        Lattice lattice;
+        if (!reader.GetNextLattice(lattice)) break;
 
-	// Choose directions
-	// For each sentence
-		// Read lattice into memory
-		// For each direction
-			// Propagate envelope through the lattice
-			// Get gamma boundaries gamma_1..gamma_n
-			// Count BLEU n-gram statistics for e_1..e_n
-	
-	// For each direction
-		// Merge gamma points
-		// Maximimize BLEU over gamma and direction
+        latticeEnvelope(lattice, dir, lambdas);
+    }
+}
 
-	// Update lambdas
+int main(int argc, char **argv)
+{
+    // Read options
 
-	return 0;
+    // Choose directions
+    // For each sentence
+        // Read lattice into memory
+        // For each direction
+            // Propagate envelope through the lattice
+            // Get gamma boundaries gamma_1..gamma_n
+            // Count BLEU n-gram statistics for e_1..e_n
+
+    // For each direction
+        // Merge gamma points
+        // Maximimize BLEU over gamma and direction
+
+    // Update lambdas
+
+    test1();
+
+    return 0;
 }
 
