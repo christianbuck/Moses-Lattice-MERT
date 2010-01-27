793c7c9c4f10d474485b0ffa39de5cf4f6858700
kgoba <karlis.goba@gmail.com>
2010-01-27 10:01:04 +0000

    Added some comments

--- SweepLine.cpp
+++ SweepLine.cpp
@@ -7,9 +7,10 @@ using std::numeric_limits;
 
 struct Line
 {
-    double  x;
-    double  y;
-    double  m;
+    // describes a line y = y0 + m * x
+    double  x;      // x intercept with the left adjacent line
+    double  y0;     // y intercept with x = 0 (y-offset)
+    double  m;      // slope coefficient
     size_t  index;
     
     static bool CompareBySlope(const Line &a, const Line &b)
@@ -17,6 +18,11 @@ struct Line
         return a.m < b.m;
     }
 };
+
+
+// Implementation of Algorithm 1
+// Calculates the upper envelope for a set of lines 
+// The algorithm modifies the line array in place
 
 void algorithm1(vector<Line> &a)
 {
@@ -29,11 +35,11 @@ void algorithm1(vector<Line> &a)
         l.x = -numeric_limits<double>::max();
         if (0 < j) {
             if (a[j - 1].m == l.m) {
-                if (l.y <= a[j - 1].y) continue;
+                if (l.y0 <= a[j - 1].y0) continue;
                 --j;
             }
             while (0 < j) {
-                l.x = (l.y - a[j - 1].y) / (a[j - 1].m - l.m);
+                l.x = (l.y0 - a[j - 1].y0) / (a[j - 1].m - l.m);
                 if (a[j - 1].x < l.x) break;
                 --j;
             }
