75c48be5f1020f980f3a59048682b5351c319111
kgoba <karlis.goba@gmail.com>
2010-01-29 19:55:34 +0000

    New lambdas for faster hill-climbing

--- runMERT.sh
+++ runMERT.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-time ./LatticeMERT -m 3 -l "-0.21 1.0 0.02 0.05 0.01 0.21 0.05 0.05 0.15 0.08 0.02 0.05 0.06 0.03 0.01" -r case4.ref case4.osg
+LAMBDAS="-0.01 1 0.02 0.037222 0.01 0.21 0.05 0.131572 0.15 0.08 0.0615917 0.05 0.06 0.16 0.01"
+
+time ./LatticeMERT -m 20 -l "$LAMBDAS" -r case4.ref case4.osg
 
